0bf40ab1513ca59e165a8c735f368bda4e0d7df7
phatdroid94 <phatdroid94@gmail.com>
2020-06-11 20:42:28 +0700

    dang ky tai khoan bang email/password

--- app/src/main/java/com/example/authentication25022020/MainActivity.java
+++ app/src/main/java/com/example/authentication25022020/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.authentication25022020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText mEdtEmail,mEdtPass;
+    EditText mEdtEmail, mEdtPass;
     Button mBtnDangnhap,
             mBtnDangky,
             mBtnThongTin,
             mBtnCapNhatThongTin,
-            mBtnResetMk,mBtnXacThuc;
+            mBtnResetMk, mBtnXacThuc;
+    FirebaseAuth mAuth;
+    // mail ao : android2502@yopmail.com
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +37,28 @@ public class MainActivity extends AppCompatActivity {
         mBtnCapNhatThongTin = findViewById(R.id.buttonCapnhat);
         mBtnResetMk = findViewById(R.id.buttonResetpassword);
         mBtnXacThuc = findViewById(R.id.buttonVerification);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mBtnDangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEdtEmail.getText().toString();
+                String password = mEdtPass.getText().toString();
+//                validation : firebase auto check
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this, "Dang ky thanh cong", Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Log.d("BBB",task.getException().getMessage());
+                                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
